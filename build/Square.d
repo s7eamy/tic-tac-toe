fd9f11afd43a488323a493bc3b8b53800dc789d9
s7eamy <aurimasotaku225@gmail.com>
2021-02-27 00:09:16 +0200

    starting to implement AI, move lib into project dir

---
+++ build/Square.d
@@ -0,0 +1,146 @@
+build/Square.o: src/Square.cpp include/mingw_dev_lib/include/SDL2/SDL.h \
+ include/mingw_dev_lib/include/SDL2/SDL_main.h \
+ include/mingw_dev_lib/include/SDL2/SDL_stdinc.h \
+ include/mingw_dev_lib/include/SDL2/SDL_config.h \
+ include/mingw_dev_lib/include/SDL2/SDL_platform.h \
+ include/mingw_dev_lib/include/SDL2/begin_code.h \
+ include/mingw_dev_lib/include/SDL2/close_code.h \
+ include/mingw_dev_lib/include/SDL2/SDL_assert.h \
+ include/mingw_dev_lib/include/SDL2/SDL_atomic.h \
+ include/mingw_dev_lib/include/SDL2/SDL_audio.h \
+ include/mingw_dev_lib/include/SDL2/SDL_error.h \
+ include/mingw_dev_lib/include/SDL2/SDL_endian.h \
+ include/mingw_dev_lib/include/SDL2/SDL_mutex.h \
+ include/mingw_dev_lib/include/SDL2/SDL_thread.h \
+ include/mingw_dev_lib/include/SDL2/SDL_rwops.h \
+ include/mingw_dev_lib/include/SDL2/SDL_clipboard.h \
+ include/mingw_dev_lib/include/SDL2/SDL_cpuinfo.h \
+ include/mingw_dev_lib/include/SDL2/SDL_events.h \
+ include/mingw_dev_lib/include/SDL2/SDL_video.h \
+ include/mingw_dev_lib/include/SDL2/SDL_pixels.h \
+ include/mingw_dev_lib/include/SDL2/SDL_rect.h \
+ include/mingw_dev_lib/include/SDL2/SDL_surface.h \
+ include/mingw_dev_lib/include/SDL2/SDL_blendmode.h \
+ include/mingw_dev_lib/include/SDL2/SDL_keyboard.h \
+ include/mingw_dev_lib/include/SDL2/SDL_keycode.h \
+ include/mingw_dev_lib/include/SDL2/SDL_scancode.h \
+ include/mingw_dev_lib/include/SDL2/SDL_mouse.h \
+ include/mingw_dev_lib/include/SDL2/SDL_joystick.h \
+ include/mingw_dev_lib/include/SDL2/SDL_gamecontroller.h \
+ include/mingw_dev_lib/include/SDL2/SDL_sensor.h \
+ include/mingw_dev_lib/include/SDL2/SDL_quit.h \
+ include/mingw_dev_lib/include/SDL2/SDL_gesture.h \
+ include/mingw_dev_lib/include/SDL2/SDL_touch.h \
+ include/mingw_dev_lib/include/SDL2/SDL_filesystem.h \
+ include/mingw_dev_lib/include/SDL2/SDL_haptic.h \
+ include/mingw_dev_lib/include/SDL2/SDL_hints.h \
+ include/mingw_dev_lib/include/SDL2/SDL_loadso.h \
+ include/mingw_dev_lib/include/SDL2/SDL_log.h \
+ include/mingw_dev_lib/include/SDL2/SDL_messagebox.h \
+ include/mingw_dev_lib/include/SDL2/SDL_metal.h \
+ include/mingw_dev_lib/include/SDL2/SDL_power.h \
+ include/mingw_dev_lib/include/SDL2/SDL_render.h \
+ include/mingw_dev_lib/include/SDL2/SDL_shape.h \
+ include/mingw_dev_lib/include/SDL2/SDL_system.h \
+ include/mingw_dev_lib/include/SDL2/SDL_timer.h \
+ include/mingw_dev_lib/include/SDL2/SDL_version.h \
+ include/mingw_dev_lib/include/SDL2/SDL_locale.h \
+ include/mingw_dev_lib/include/SDL2/SDL_misc.h include/Square.hpp
+
+include/mingw_dev_lib/include/SDL2/SDL.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_main.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_stdinc.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_config.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_platform.h:
+
+include/mingw_dev_lib/include/SDL2/begin_code.h:
+
+include/mingw_dev_lib/include/SDL2/close_code.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_assert.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_atomic.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_audio.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_error.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_endian.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_mutex.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_thread.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_rwops.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_clipboard.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_cpuinfo.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_events.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_video.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_pixels.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_rect.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_surface.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_blendmode.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_keyboard.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_keycode.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_scancode.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_mouse.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_joystick.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_gamecontroller.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_sensor.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_quit.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_gesture.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_touch.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_filesystem.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_haptic.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_hints.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_loadso.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_log.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_messagebox.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_metal.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_power.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_render.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_shape.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_system.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_timer.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_version.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_locale.h:
+
+include/mingw_dev_lib/include/SDL2/SDL_misc.h:
+
+include/Square.hpp:
